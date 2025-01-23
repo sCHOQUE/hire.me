f85a661679b5a98da4337bda6b2142b373fb3155
Yagod <yagodossantos14@hotmail.com>
2025-01-23 15:25:49 -0300

    Update remote

---
+++ url-shortener-app/src/test/java/com/yagod/url_shortener_app/UrlShortenerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagod.url_shortener_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
