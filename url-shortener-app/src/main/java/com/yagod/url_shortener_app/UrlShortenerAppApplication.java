7f9b1ae88c2082a453fbe535dd096aac76ecdfde
Yagod <yagodossantos14@hotmail.com>
2025-01-23 15:12:03 -0300

    general add

---
+++ url-shortener-app/src/main/java/com/yagod/url_shortener_app/UrlShortenerAppApplication.java
@@ -0,0 +1,13 @@
+package com.yagod.url_shortener_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerAppApplication.class, args);
+	}
+
+}
